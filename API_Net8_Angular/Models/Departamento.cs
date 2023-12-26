89a3331729e75108ac2ed261918cec43e4b07527
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-26 17:33:46 -0300

    Model Departamento

--- API_Net8_Angular/Models/Departamento.cs
+++ API_Net8_Angular/Models/Departamento.cs
@@ -1,9 +1,36 @@
-﻿namespace GestionRecursosHumanos.API.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace GestionRecursosHumanos.API.Models
 {
     /// <summary>
-    /// 
+    /// Clase que representa un departamento.
     /// </summary>
     public class Departamento
     {
+        /// <summary>
+        /// Obtiene o establece el identificador del departamento.
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el nombre del departamento.
+        /// </summary>
+        [BsonElement("nombre")]
+        public required string Nombre { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece la descripción del departamento.
+        /// </summary>
+        [BsonElement("descripcion")]
+        public required string Descripcion { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece la ubicación del departamento.
+        /// </summary>
+        [BsonElement("ubicacion")]
+        public required string Ubicacion { get; set; }
     }
 }

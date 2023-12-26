553b7cd5a585086816d8dcc853f789bfa00ca357
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-26 17:27:34 -0300

    Class Empleado

--- API_Net8_Angular/Models/Empleado.cs
+++ API_Net8_Angular/Models/Empleado.cs
@@ -1,10 +1,49 @@
-﻿namespace GestionRecursosHumanos.API.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace GestionRecursosHumanos.API.Models
 {
     /// <summary>
-    /// 
+    /// Clase que representa a un empleado.
     /// </summary>
     public class Empleado
     {
+        /// <summary>
+        /// Id empleado
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
 
+        /// <summary>
+        /// Obtiene o establece el nombre del empleado.
+        /// </summary>
+        [BsonElement("nombre")]
+        public required string Nombre { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el apellido del empleado.
+        /// </summary>
+        [BsonElement("apellido")]
+        public required string Apellido { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el correo electrónico del empleado.
+        /// </summary>
+        [BsonElement("email")]
+        public required string Email { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el cargo del empleado.
+        /// </summary>
+        [BsonElement("cargo")]
+        public required string Cargo { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el ID del departamento al que pertenece el empleado.
+        /// </summary>
+        [BsonElement("departamento")]
+        public ObjectId DepartamentoId { get; set; }
     }
+
 }

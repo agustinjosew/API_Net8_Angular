fd56b3a20aee1d15ee2652187496682fb1a617e7
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-26 12:49:15 -0300

    Extend Swagger configs parameters

--- API_Net8_Angular/Program.cs
+++ API_Net8_Angular/Program.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 
@@ -15,11 +14,50 @@ namespace API_Net8_Angular
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen( c => {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v1" });
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    "v1", new OpenApiInfo
+                    {
+                        Title = "Mi API",
+                        Version = "v1",
+                        Description = "Gestión de recursos humanos, diseñada para facilitar la administración eficiente de empleados, candidatos, puestos, departamentos, y gestión de vacaciones y ausencias. ",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Agustin Jose Wawrzyk",
+                            Email = "Test",
+                            Url = new Uri("https://soundcloud.com/awakenings/awakenings-spring-festival-2023-bart-skills")
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Licencia API",
+                            Url = new Uri("https://api.com/licencia")
+                        }
+                    });
 
                 // Configuración para JWT en Swagger
-                // ...
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Por favor, inserte el token JWT con 'Bearer ' prefijo",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
 
                 // Habilitar anotaciones XML
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
@@ -28,10 +66,10 @@ namespace API_Net8_Angular
 
                 // Filtros personalizados
                 // ...
-                
+
             });
 
-                var app = builder.Build();
+            var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())

1daf73278f60adfa89e72a6517996efccb9cab10
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-26 12:16:25 -0300

    Swashbuckle added

--- API_Net8_Angular/Program.cs
+++ API_Net8_Angular/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace API_Net8_Angular
 {
     public class Program
@@ -12,9 +15,23 @@ namespace API_Net8_Angular
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen( c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v1" });
 
-            var app = builder.Build();
+                // Configuración para JWT en Swagger
+                // ...
+
+                // Habilitar anotaciones XML
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+                // Filtros personalizados
+                // ...
+                
+            });
+
+                var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())

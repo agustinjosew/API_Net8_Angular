b7b6bcd2f16450f13f61945504848eb68308b25d
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-27 11:58:20 -0300

    Work on EmpleadoRepository

---
+++ API_Net8_Angular/Repositories/EmpleadoRepository.cs
@@ -0,0 +1,42 @@
+﻿using GestionRecursosHumanos.API.Models;
+using GestionRecursosHumanos.API.Repositories.Interfaces;
+using MongoDB.Driver;
+
+namespace GestionRecursosHumanos.API.Repositories
+{
+    public class EmpleadoRepository : IEmpleadoRepository
+    {
+        private readonly IMongoCollection<Empleado> _empleados;
+
+        public EmpleadoRepository(IMongoClient client)
+        {
+            var database = client.GetDatabase("gestionRecursosHumanosDB");
+            _empleados = database.GetCollection<Empleado>("Empleados");
+        }
+
+        public async Task<IEnumerable<Empleado>> GetAllAsync()
+        {
+            return await _empleados.Find(empleado => true).ToListAsync();
+        }
+
+        Task IEmpleadoRepository.AddAsync(Empleado empleado)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IEmpleadoRepository.DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Empleado> IEmpleadoRepository.GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IEmpleadoRepository.UpdateAsync(string id, Empleado empleado)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

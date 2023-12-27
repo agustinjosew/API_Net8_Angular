daa4792a32d9c58ea6fa2d84c9a08b8e12222a34
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-27 18:11:11 -0300

    DepartamentoDTO created

---
+++ API_Net8_Angular/DTOs/DepartamentoDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace GestionRecursosHumanos.API.DTOs
+{
+    /// <summary>
+    /// DTO que representa un departamento.
+    /// </summary>
+    public class DepartamentoDTO
+    {
+        /// <summary>
+        /// Obtiene o establece el identificador del departamento.
+        /// </summary>
+        public required string Id { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el nombre del departamento.
+        /// </summary>
+        public required string Nombre { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece la descripción del departamento.
+        /// </summary>
+        public required string Descripcion { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece la ubicación del departamento.
+        /// </summary>
+        public required string Ubicacion { get; set; }
+    }
+}

6f311fff31f215e67ea4953230d9eb60114dd7ac
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-28 21:26:34 -0300

    Creation of the component for Empleado-List

--- Front_Angular/src/app/app.module.ts
+++ Front_Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { EmpleadoListComponent } from './components/empleado-list/empleado-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoListComponent
   ],
   imports: [
     BrowserModule,

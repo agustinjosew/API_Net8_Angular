cbe77f96a7dd600d149f1ccb4740d1a8a9defa1a
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-31 08:53:21 -0300

    Angular implementation for the Empleado form and list components

--- Front_Angular/src/app/components/empleado-form/empleado-form.component.ts
+++ Front_Angular/src/app/components/empleado-form/empleado-form.component.ts
@@ -1,4 +1,8 @@
+// src/app/components/empleado-form/empleado-form.component.ts
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmpleadoService } from '../../services/empleado.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-empleado-form',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./empleado-form.component.css']
 })
 export class EmpleadoFormComponent {
+  empleadoForm: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private empleadoService: EmpleadoService,
+    private router: Router
+  ) {
+    this.empleadoForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      cargo: ['', Validators.required],
+      departamentoId: ['', Validators.required],
+      puestoId: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.empleadoForm.valid) {
+      this.empleadoService.createEmpleado(this.empleadoForm.value).subscribe(() => {
+        this.router.navigate(['/empleados']); // Redirige al usuario a la lista de empleados
+      });
+    }
+  }
 }
